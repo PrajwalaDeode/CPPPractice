a6d3eaf97ded2011f9dff88c286cd669199258bc
PrajwalaDeode <86475487+PrajwalaDeode@users.noreply.github.com>
2021-06-26 21:45:44 -0700

    Add files via upload

---
+++ Number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1 = 90;
+	double num2 = 34.65;
+	char c = 'A';
+
+	//Printing the numbers
+
+	cout << num1 << endl;
+	cout << num2 << endl;
+	cout << "Character: " << c << endl;
+	return 0;
+}
